2f6bebd4534f213a8c6bdd793b831abdcf605fbb
Akram hossain <55057902+engineertuhin@users.noreply.github.com>
2022-06-11 01:58:16 +0600

    Update database.php

--- classes/database/database.php
+++ classes/database/database.php
@@ -1,10 +1,10 @@
 <?php
 
 class db{
-private $host="localhost";
-private $user="root";
-private $pass="";
-private $database="messenger";
+private $host="db4free.net";
+private $user="tuhin1191";
+private $pass="tuhin1191";
+private $database="tuhin1191";
 public function connection(){
 
  return new mysqli($this->host,$this->user,$this->pass,$this->database);
